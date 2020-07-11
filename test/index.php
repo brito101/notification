feecf17fb5b8bbddcc27129709ca60bb35bd5232
Rodrigo Brito <rodrigobrito101@hotmail.com>
2020-07-11 20:41:22 -0300

    Library OK

--- test/index.php
+++ test/index.php
@@ -5,6 +5,6 @@ require __DIR__ . '/../lib_ext/autoload.php';
 use Notification\Email;
 
 $novoEmail = new Email('2',  'smtp.live.com', 'rodrigobrito101@hotmail.com', '***********', 587, 'rodrigobrito101@hotmail.com', 'Rodrigo Brito');
-$novoEmail->sendEmail("Teste", "<p>Texto</p>", "rodrigobrito101@hotmail.com", "Rodrigo Brito", "rodrigobrito101@gmail.com", "Rodrigo");
+$novoEmail->sendEmail("Teste", "<p>Text in HTML</p>", "rodrigobrito101@hotmail.com", "Rodrigo Brito", "rodrigobrito101@gmail.com", "Rodrigo Brito");
 
 var_dump($novoEmail);
